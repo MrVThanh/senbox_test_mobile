38bb4d73a0a638d7ac5744cd3debea4946f17bf6
epoffice14 <epoffice14@Thanhweb.local>
2024-11-11 16:00:29 +0700

    feat: scan QR code

---
+++ android/app/src/main/java/com/vercel/senbox/MainActivity.java
@@ -0,0 +1,5 @@
+package com.vercel.senbox;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vercel.senbox',
+  appName: 'senbox_nextmobile',
+  webDir: 'out'
+};
+
+export default config;
